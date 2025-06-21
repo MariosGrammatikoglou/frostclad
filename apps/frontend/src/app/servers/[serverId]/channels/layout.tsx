c54ae0a15d15187bc1833250b9675c517aa004a6
Marios <grammamarios@outlook.com>
2025-06-21 22:03:55 +0300

    Dsad

--- apps/frontend/src/app/servers/[serverId]/channels/layout.tsx
+++ apps/frontend/src/app/servers/[serverId]/channels/layout.tsx
@@ -5,7 +5,7 @@ import ChannelSidebar from '@/components/ChannelSidebar';
 
 export default function ChannelsLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div style={{ display: 'flex', height: '100%', width: '100%' }}>
+        <div style={{ display: 'flex', height: '70%', width: '70%' }}>
             {/* Server Sidebar */}
             <div style={{ width: 240, minWidth: 200, height: "100%" }}>
                 <Sidebar />
